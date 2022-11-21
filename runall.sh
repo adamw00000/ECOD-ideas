f6e079a32ed00f353dcdb3418b6d46a5187d8c0d
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2022-11-21 21:24:18 +0100

    Corrected run script

--- runall.sh
+++ runall.sh
@@ -1,10 +1,11 @@
-python occ-tests-binary-fdr.py
-python occ-tests-binary-general.py
-python occ-tests-binary-t1e.py
-python occ-tests-dataset-fdr.py
-python occ-tests-dataset-general.py
-python occ-tests-dataset-t1e.py
-python occ-tests-dist-fdr.py
-python occ-tests-dist-general.py
-python occ-tests-dist-t1e.py
-
+python ./occ-tests-binary-fdr.py
+python ./occ-tests-binary-general.py
+python ./occ-tests-binary-t1e.py
+python ./occ-tests-dataset-fdr.py
+python ./occ-tests-dataset-general.py
+python ./occ-tests-dataset-t1e.py
+python ./occ-tests-dist-fdr.py
+python ./occ-tests-dist-general.py
+python ./occ-tests-dist-t1e.py
+tar -czf results.tar.gz results*/
+echo "Done"
