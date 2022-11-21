b6bf8ed9f0379817e91a862dffda27b86bf4ee94
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2022-11-21 10:26:19 +0100

    Requirements + runall script

---
+++ runall.sh
@@ -0,0 +1,10 @@
+python occ-tests-binary-fdr.py
+python occ-tests-binary-general.py
+python occ-tests-binary-t1e.py
+python occ-tests-dataset-fdr.py
+python occ-tests-dataset-general.py
+python occ-tests-dataset-t1e.py
+python occ-tests-dist-fdr.py
+python occ-tests-dist-general.py
+python occ-tests-dist-t1e.py
+
