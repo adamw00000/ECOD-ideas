318f14cf495ddd07027af18d99396fe20fdf9f6d
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2022-11-22 11:11:45 +0100

    Added tee to runall script

--- runall.sh
+++ runall.sh
@@ -1,11 +1,11 @@
-python ./occ-tests-binary-fdr.py
-python ./occ-tests-binary-general.py
-python ./occ-tests-binary-t1e.py
-python ./occ-tests-dataset-fdr.py
-python ./occ-tests-dataset-general.py
-python ./occ-tests-dataset-t1e.py
-python ./occ-tests-dist-fdr.py
-python ./occ-tests-dist-general.py
-python ./occ-tests-dist-t1e.py
-tar -czf results.tar.gz results*/
-echo "Done"
+python -u ./occ-tests-binary-fdr.py | tee log.txt
+python -u ./occ-tests-binary-general.py | tee -a log.txt
+python -u ./occ-tests-binary-t1e.py | tee -a log.txt
+python -u ./occ-tests-dataset-fdr.py | tee -a log.txt
+python -u ./occ-tests-dataset-general.py | tee -a log.txt
+python -u ./occ-tests-dataset-t1e.py | tee -a log.txt
+python -u ./occ-tests-dist-fdr.py | tee -a log.txt
+python -u ./occ-tests-dist-general.py | tee -a log.txt
+python -u ./occ-tests-dist-t1e.py | tee -a log.txt
+tar -czf results.tar.gz results*/ | tee -a log.txt
+echo "Done" | tee -a log.txt
