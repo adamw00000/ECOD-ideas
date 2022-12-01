9308ffccfb1f3e8b9c53b82fc02fcd50f4c92fac
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2022-12-01 14:09:20 +0100

    Re-enabled single-repeat multisplit

--- occ_test_base_t1e.py
+++ occ_test_base_t1e.py
@@ -28,7 +28,7 @@ def get_cutoffs(construct_clf, alpha, resampling_repeats) -> List[Cutoff]:
         # BootstrapThresholdCutoff(construct_clf, resampling_repeats),
         # MultisplitThresholdCutoff(construct_clf, resampling_repeats),
         # MultisplitCutoff(construct_clf, alpha, resampling_repeats=resampling_repeats, median_multiplier=2),
-        # MultisplitCutoff(construct_clf, alpha, resampling_repeats=1, median_multiplier=2),
+        MultisplitCutoff(construct_clf, alpha, resampling_repeats=1, median_multiplier=1),
         MultisplitCutoff(construct_clf, alpha, resampling_repeats=resampling_repeats, median_multiplier=1),
         NoSplitCutoff(construct_clf, alpha),
     ]
