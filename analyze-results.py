b80f6a1cf29c2c4a0dda38203c7068904ac146d5
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2023-01-13 13:38:15 +0100

    Expanded analysis script

--- analyze-results.py
+++ analyze-results.py
@@ -3,26 +3,50 @@ import os
 import pandas as pd
 import numpy as np
 
-RESULTS_ROOT = 'results-2023-01-04'
+RESULTS_ROOT = 'results-2023-01-07'
 
-for_df = pd.read_csv(
-    os.path.join(
-        RESULTS_ROOT,
-        'results_BINARYdata_fdr_0.10',
-        'BINARYdata-all-FOR-mean.csv'
-    ),
-    header=[0, 1],
-    index_col=[0]
-)
-for_df
+metrics = ['FOR', 'FDR', 'AUC']
+
+metric_dfs = {}
+for metric in metrics:
+    metric_df = pd.read_csv(
+        os.path.join(
+            RESULTS_ROOT,
+            'results_BINARYdata_fdr_0.10',
+            f'BINARYdata-all-{metric}-mean.csv'
+        ),
+        header=[0, 1],
+        index_col=[0]
+    )
+    metric_dfs[metric] = metric_df
+
+for metric in metrics:
+    metric_df = metric_dfs[metric]
+    for_ctl_methods = [
+        (method, cutoff) for method, cutoff in metric_df.columns \
+        if 'Multisplit+FOR-CTL' in cutoff
+    ]
+    metric_df = metric_df[for_ctl_methods]
+    metric_dfs[metric] = metric_df.iloc[:-1, :-3]
+
+metric_dfs['FOR'] = metric_dfs['FOR'] \
+    .sort_values([('IForest', 'Multisplit+FOR-CTL')])
+
+# (metric_dfs['FOR'] < 0.2).sum(axis=1).reset_index(drop=False) \
+#     .sort_values(0)
+# (metric_dfs['FOR'] < 0.2).sum(axis=0)
+metric_dfs['FOR']
 
 # %%
-for_ctl_methods = [
-    (method, cutoff) for method, cutoff in for_df.columns \
-    if 'Multisplit+FOR-CTL' in cutoff
-]
-for_df = for_df[for_ctl_methods]
-for_df = for_df.iloc[:-1]
-for_df.sort_values([('IForest', 'Multisplit+FOR-CTL')])
+dataset_order = metric_dfs['FOR'].index
+dataset_order
+
+# %%
+for metric in metrics:
+    metric_dfs[metric] = metric_dfs[metric] \
+        .loc[dataset_order]
+
+# %%
+metric_dfs['AUC']
 
 # %%
