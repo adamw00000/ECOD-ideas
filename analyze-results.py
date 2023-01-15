18d0e5095306fa0313bd5cf6b7f829671dd66f9c
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2023-01-15 18:37:43 +0100

    FOR table

--- analyze-results.py
+++ analyze-results.py
@@ -5,7 +5,10 @@ import numpy as np
 
 RESULTS_ROOT = 'results-2023-01-07'
 
-metrics = ['FOR', 'FDR', 'AUC']
+pd.set_option('display.max_colwidth', 1000)
+metrics = ['FOR']
+
+alpha = 0.1
 
 metric_dfs = {}
 for metric in metrics:
@@ -18,7 +21,24 @@ for metric in metrics:
         header=[0, 1],
         index_col=[0]
     )
-    metric_dfs[metric] = metric_df
+    sem_df = pd.read_csv(
+        os.path.join(
+            RESULTS_ROOT,
+            'results_BINARYdata_fdr_0.10',
+            f'BINARYdata-all-{metric}-sem.csv'
+        ),
+        header=[0, 1],
+        index_col=[0]
+    )
+        
+    metric_dfs[metric] = '$' \
+        + metric_df.applymap('{:.3f}'.format) \
+        + ' \pm ' \
+        + sem_df.applymap('{:.3f}'.format) \
+        + '\\,' \
+        + np.where(metric_df <= 2 * alpha, '\\textcolor{black}{\\checkmark}', '\\phantom{\\checkmark}') \
+        + np.where(metric_df <= alpha, '\\textcolor{magenta}{\\checkmark}', '\\phantom{\\checkmark}')\
+        + '$'
 
 for metric in metrics:
     metric_df = metric_dfs[metric]
@@ -32,10 +52,29 @@ for metric in metrics:
 metric_dfs['FOR'] = metric_dfs['FOR'] \
     .sort_values([('IForest', 'Multisplit+FOR-CTL')])
 
+no_cutoff_row_cols = [method for method, c in metric_dfs['FOR'].columns]
+metric_dfs['FOR'].columns = no_cutoff_row_cols
+
+metric_dfs['FOR'] = metric_dfs['FOR'][['IForest', 'A^3', 'Mahalanobis', 'ECODv2', 'ECODv2+PCA1.0']] \
+    .rename(columns={'ECODv2': 'ECOD', 'ECODv2+PCA1.0': 'ECOD + PCA', 'A^3': '$A^3$'})
+
+metric_dfs['FOR'].index = '\\textit{' \
+    + metric_dfs['FOR'].index.str.replace('\(csv\) ', '') \
+    + '}'
+metric_dfs['FOR'].columns = ['\\textbf{' + c + '}' for c in metric_dfs['FOR'].columns]
+metric_dfs['FOR'].columns.name = '\\textbf{Dataset}'
 # (metric_dfs['FOR'] < 0.2).sum(axis=1).reset_index(drop=False) \
 #     .sort_values(0)
 # (metric_dfs['FOR'] < 0.2).sum(axis=0)
-metric_dfs['FOR']
+with open(os.path.join('latex', 'for-table.tex'), 'w+') as f:
+    f.write(
+        metric_dfs['FOR'] \
+            .to_latex(
+                escape=False, 
+                # bold_rows=True,
+                column_format='l|ccccc',
+            )
+    )
 
 # %%
 dataset_order = metric_dfs['FOR'].index
